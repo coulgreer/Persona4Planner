e80a45205871ae7e516276ade03ed212c5ba639a
Coul Greer <cagreer@bsu.edu>
2017-02-20 18:37:19 -0500

    Finalized the layout for the navigation bar and card panel. Also, went through the code and did some house cleaning (got rid of magic numbers and created helper methods).

--- src/Persona4Planner.java
+++ src/Persona4Planner.java
@@ -1,41 +1,40 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Persona4Planner {
-	JPanel cards;
+	private final static String STATUS_SCREEN = "Status Screen";
+	private final static String CALENDAR_SCREEN = "Calendar Screen";
+	private final static int DEFAULT_SCREEN_WIDTH = 1200;
+	private final static int DEFAULT_SCREEN_HEIGHT = 800;
+	private final static int DEFAULT_NAVBAR_WIDTH = (int) Math.round((DEFAULT_SCREEN_WIDTH * .25));
+	private final static int DEFAULT_CARD_WIDTH = (int) Math.round((DEFAULT_SCREEN_WIDTH * .75));
+	private final static int NAVBAR_BUTTON_BUFFER = 5;
+	private final static int DEFAULT_NAVBAR_BUTTON_WIDTH = DEFAULT_NAVBAR_WIDTH - (NAVBAR_BUTTON_BUFFER * 2);
+	private final static int DEFAULT_NAVBAR_BUTTON_HEIGHT = 50;
 
-	public void addComponentToPane(Container pane) {
+	private JPanel cards;
+
+	public void addComponentToPane(Container panel) {
 		JPanel navigationPanel = new JPanel();
-		JButton statusButton, calendarButton;
-		statusButton = new JButton("Status Screen");
-		statusButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				CardLayout cardLayout = (CardLayout) cards.getLayout();
-				cardLayout.show(cards, e.getActionCommand());
-			}
-		});
 
-		calendarButton = new JButton("Calendar Screen");
-		calendarButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				CardLayout cardLayout = (CardLayout) cards.getLayout();
-				cardLayout.show(cards, e.getActionCommand());
-			}
-		});
-
+		navigationPanel.setLayout(new BoxLayout(navigationPanel, BoxLayout.Y_AXIS));
+		navigationPanel.setPreferredSize(new Dimension(DEFAULT_NAVBAR_WIDTH, DEFAULT_SCREEN_HEIGHT));
 		navigationPanel.setBackground(new Color(255, 174, 32));
-		navigationPanel.add(statusButton);
-		navigationPanel.add(calendarButton);
+		navigationPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		buildNavigationButton(STATUS_SCREEN, navigationPanel);
+		buildNavigationButton(CALENDAR_SCREEN, navigationPanel);
 
 		JPanel statusCard = new JPanel();
 		statusCard.setBackground(new Color(254, 234, 44));
@@ -44,17 +43,35 @@ public class Persona4Planner {
 		calendarCard.setBackground(Color.PINK);
 
 		cards = new JPanel(new CardLayout());
-		cards.add(statusCard, "Status Screen");
-		cards.add(calendarCard, "Calendar Screen");
+		cards.setPreferredSize(new Dimension(DEFAULT_CARD_WIDTH, DEFAULT_SCREEN_HEIGHT));
+		cards.add(statusCard, STATUS_SCREEN);
+		cards.add(calendarCard, CALENDAR_SCREEN);
 
-		pane.add(navigationPanel, BorderLayout.PAGE_START);
-		pane.add(cards, BorderLayout.CENTER);
+		panel.add(navigationPanel, BorderLayout.WEST);
+		panel.add(cards, BorderLayout.CENTER);
 
+	}
+
+	private void buildNavigationButton(String buttonName, Container panel) {
+		JButton button = new JButton(buttonName);
+		button.setMaximumSize(new Dimension(DEFAULT_NAVBAR_BUTTON_WIDTH, DEFAULT_NAVBAR_BUTTON_HEIGHT));
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cardLayout = (CardLayout) cards.getLayout();
+				cardLayout.show(cards, e.getActionCommand());
+			}
+		});
+
+		panel.add(Box.createRigidArea(new Dimension(0, 5)));
+		panel.add(button);
+		panel.add(Box.createRigidArea(new Dimension(0, 5)));
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Persona 4 Planner");
-		frame.setSize(800, 700);
+		frame.setSize(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		Persona4Planner p4p = new Persona4Planner();
